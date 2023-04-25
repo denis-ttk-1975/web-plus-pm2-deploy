a262f4f17f9ade80c44fcf2c29cfa88f64191f80
denis-ttk-1975 <denis.ttk@gmail.com>
2023-04-25 13:13:54 +0300

    repo changeв to https ver

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       user: "ddduuu1975",
       host: "84.201.165.117",
       ref: "origin/master",
-      repo: "git@github.com:denis-ttk-1975/web-plus-pm2-deploy.git",
+      repo: "https://github.com/denis-ttk-1975/web-plus-pm2-deploy",
       path: "/home/ddduuu1975/web-plus-pm2-deploy",
       "post-deploy": "cd backend && npm i && npm run build",
     },
