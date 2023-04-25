b9dd2e1444cc22081ee10b70b0488087ef4a66e7
denis-ttk-1975 <denis.ttk@gmail.com>
2023-04-25 13:38:12 +0300

    try to find backend

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       host: "84.201.165.117",
       ref: "origin/master",
       repo: "https://github.com/denis-ttk-1975/web-plus-pm2-deploy",
-      path: "/home/ddduuu1975/web-plus-pm2-deploy/backend",
+      path: "/home/ddduuu1975/web-plus-pm2-deploy/",
       "post-deploy": "cd backend && npm i && npm run build",
     },
   },
