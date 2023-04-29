83763950029cc21b16d72acb87c6c8e1f4cc8e46
denis-ttk-1975 <denis.ttk@gmail.com>
2023-04-29 21:33:38 +0300

    secrets to front config added

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -17,11 +17,11 @@ module.exports = {
   ],
   deploy: {
     production: {
-      user: "ddduuu1975",
-      host: "84.201.165.117",
-      ref: "origin/master",
-      repo: "git@github.com:denis-ttk-1975/web-plus-pm2-deploy.git",
-      path: "/home/ddduuu1975/web-plus-pm2-deploy/frontend",
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
       "pre-deploy-local": `scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
 
       "post-deploy": "cd frontend && npm i && npm run build",
