4d92e8451175d683d45090d5ddc17a31b674a058
denis-ttk-1975 <denis.ttk@gmail.com>
2023-04-29 19:48:36 +0300

    path for backend fixed

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       host: "84.201.165.117",
       ref: "origin/master",
       repo: "git@github.com:denis-ttk-1975/web-plus-pm2-deploy.git",
-      path: "/home/ddduuu1975/web-plus-pm2-deploy/frontend",
+      path: "/home/ddduuu1975/web-plus-pm2-deploy/backend",
       "post-deploy":
         "touch METKA.FILE && cd backend && touch METKA1.FILE1 && npm i && npm run build && pm2 startOrRestart ecosystem.config.js",
     },
