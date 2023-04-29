7e8d6de7d12f22842e06dee35d02f289d0699da8
denis-ttk-1975 <denis.ttk@gmail.com>
2023-04-29 19:46:11 +0300

    ecosystem files fixed

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -10,8 +10,8 @@ module.exports = {
       user: "ddduuu1975",
       host: "84.201.165.117",
       ref: "origin/master",
-      repo: "https://github.com/denis-ttk-1975/web-plus-pm2-deploy",
-      path: "/home/ddduuu1975/web-plus-pm2-deploy",
+      repo: "git@github.com:denis-ttk-1975/web-plus-pm2-deploy.git",
+      path: "/home/ddduuu1975/web-plus-pm2-deploy/frontend",
       "post-deploy":
         "touch METKA.FILE && cd backend && touch METKA1.FILE1 && npm i && npm run build && pm2 startOrRestart ecosystem.config.js",
     },
